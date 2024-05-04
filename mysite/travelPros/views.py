2f1bab14e2d761582811163521a5ea42e1ec2dea
Pol <polinboston@gmail.com>
2024-05-04 21:01:06 +0200

    working

--- mysite/travelPros/views.py
+++ mysite/travelPros/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BookingForm
+from django.http import HttpResponse
 
 # Create your views here.
 
-
-from django.http import HttpResponse
-
 def home(request):
     return render(request, 'home.html')
+
