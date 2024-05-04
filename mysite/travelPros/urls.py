6035d2cbb69593bc4399823e9578d30c782da641
Pol <polinboston@gmail.com>
2024-05-04 22:25:52 +0200

    working with data flow

--- mysite/travelPros/urls.py
+++ mysite/travelPros/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('activity/', views.activity, name='add_activity'),
+    path('companion/', views.companionview, name='companion'),
 ]
--- mysite/travelPros/views.py
+++ mysite/travelPros/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import ActivityForm
+from .models import Traveler
 from django.http import HttpResponse
 
 # Create your views here.
@@ -10,4 +11,22 @@ def home(request):
 
 
 def activity(request):
-    return render(request, 'activity.html')
+
+    form = ActivityForm()
+    context = {'form': form}
+
+    if request.method == 'POST':
+        form = ActivityForm(request.POST)
+        if form.is_valid():
+            Traveler.objects.all().delete()
+            form.save()
+            print(Traveler.objects.all())
+            return redirect('companion')
+        else:
+            return HttpResponse('Invalid data')
+    return render(request, 'activity.html', context)
+
+
+def companionview(request):
+    users = Traveler.objects.all()  # Query all activities
+    return render(request, 'companion.html', {'users': users})
