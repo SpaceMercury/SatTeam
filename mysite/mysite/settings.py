7fc3cc9bd7244acfa66dfc7a6d30ec04ad2a4bb1
Pol <polinboston@gmail.com>
2024-05-15 17:43:14 +0200

    settings update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,13 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w782)w#u(kdc(ef(!6r)-b+%c#w0hbkejiq2q_0^cfuj6g$f-a"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = [,'smokecana.biz', 'www.smokecana.biz', '127.0.0.1', 'localhost']
-
-
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
 
 # Application definition
